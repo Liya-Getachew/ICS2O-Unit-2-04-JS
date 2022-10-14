ea3db4c4d21dc12cac895e3948c27f26fdcdcfff
Liya-Getachew <Liya-Getachew@users.noreply.github.com>
2022-10-14 18:35:47 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
-  * This function calculates area of a triangle.
-  */
+ * This function calculates area of a triangle.
+ */
 function calculateArea() {
   // input
-  const base = parseInt(document.getElementById('base-of-triangle').value)
-  const height = parseInt(document.getElementById('height-of-triangle').value)
+  const base = parseInt(document.getElementById("base-of-triangle").value)
+  const height = parseInt(document.getElementById("height-of-triangle").value)
 
   // process
-  const area = (base * height)/2
+  const area = (base * height) / 2
 
   // output
-  document.getElementById('area').innerHTML = 'The area is: ' + area + ' cm²'
+  document.getElementById("area").innerHTML = "The area is: " + area + " cm²"
 }
