4c2f42b1b2086029f13d71d8722df56fabc76a64
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-14 18:33:52 +0000

    calculator setup

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+  * This function calculates area of a triangle.
+  */
+function calculateArea() {
+  // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = (base * height)/2
+
+  // output
+  document.getElementById('area').innerHTML = 'The area is: ' + area + ' cm²'
+}
